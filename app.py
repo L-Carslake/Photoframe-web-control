a8720619ae61cd1c05c3fbb3d6bcb00e032cf9b4
L-Carslake <L-Carslake@users.noreply.github.com>
2020-04-26 15:34:28 +0100

    Working site

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 import os
 from pykeyboard import PyKeyboard
+import time
 
 os.system("export DISPLAY=:0")
 keyboard = PyKeyboard()
@@ -14,9 +15,21 @@ def index():
             next_image()
         if request.form.get('prev'):
             previous_image()
-        return render_template("index.html")
+    time.sleep(0.4) # Waits for image to be reloaded
+    return render_template("index.html", current_image_name=get_current_image_name())
 
-    return render_template("index.html")
+
+# Custom static data
+@app.route('/current_image')
+def current_image():
+    return send_from_directory('/Images', get_current_image_name())
+
+
+# Custom static data
+@app.route('/cdn/<path:filename>')
+def custom_static(filename):
+    #TODO: Make path safe
+    return send_from_directory('/Images', filename)
 
 
 def next_image():
@@ -38,4 +51,14 @@ def previous_image():
 def update_current_image_file():
     keyboard.type_string('0')
     return 0
+
+
+def get_current_image_name():
+    # Update the "/Images/currentImage.txt" file and read
+    # Out: Path and file name of current photo (string)
+    update_current_image_file()
+    path = open("/Images/currentImage.txt", "r").read().strip()
+    split_path = path.split("/")
+    print(split_path[2])
+    return split_path[2]
 
