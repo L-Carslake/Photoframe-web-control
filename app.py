7e9a498572e94c8b508ad9080e8f3f51d170872a
l_carslake <lawrence@Carslake.me>
2020-04-26 22:09:09 +0000

    Added API for homeassistant. use as volumio media player
    	modified:   app.py

--- app.py
+++ app.py
@@ -34,6 +34,65 @@ def custom_static(filename):
     return send_from_directory('/Images', filename)
 
 
+
+# Below is API for homeassistant integration
+@app.route('/api/v1/commands/')
+def commands():
+    command = request.args.get('cmd')
+    if command == 'next':
+        # Next
+        next_image()
+        print('Next')
+        return ""
+    if command == 'prev':
+        # Previous
+        previous_image()
+        print('Previous')
+        return ""
+    if command  in ["play", "pause", "toggle", "stop", "volume", "playplaylist", "repeat", "random", "clearQueue", "playplaylist"]:
+        # Unimplemented commands
+        print('Unimplemented command called')
+        return ""
+    return "", 404
+
+
+@app.route('/api/v1/getState/')
+def getstate():
+    update_current_image_file()
+    time.sleep(0.4) # Waits for image to be reloaded
+    state = {  "status": "play",
+               "position": 2,
+               "title": "Photoframe",
+               "artist": get_current_image_name(),
+               "album": "",
+               "albumart": "/cdn/" + get_current_image_name(),
+               "uri": "",
+               "trackType": "",
+               "seek": 53057,
+               "duration": 809,
+               "samplerate": "96 kHz",
+               "bitdepth": "24 bit",
+               "channels": 2,
+               "random": False,
+               "repeat": False,
+               "repeatSingle": False,
+               "consume": False,
+               "volume": 43,
+               "disableVolumeControl": False,
+               "mute": False,
+               "stream": "flac",
+               "updatedb": False,
+               "volatile": False,
+               "service": "mpd"
+             }
+    return state
+
+
+@app.route('/api/v1/listplaylists/')
+def list_playlists():
+    return {}
+
+
 def next_image():
     # Next image
     keyboard.tap_key(keyboard.right_key)
